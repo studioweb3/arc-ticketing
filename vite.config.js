4be8b8507c92846f7b5335b141eed56a8d8cab4b
fabbyfab <fab.zouk@hotmail.fr>
2026-05-21 18:00:01 +0200

    Premier commit

---
+++ vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// Configuration standard pour une Web App React
+export default defineConfig({
+  plugins: [react()],
+})
